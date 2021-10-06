93d4f4180387c0dc522aaa176d3841ffb737240e
nmso2 <nmsubho@gmail.com>
2021-10-06 12:46:51 +0600

    complete firebade auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import firebaseInitialize from './Firebase/firebase.initialize';
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, signOut } from "firebase/auth";
+import { useState } from 'react';
+
+
+firebaseInitialize();
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
 function App() {
+
+  const [user, setUser] = useState({});
+  const auth = getAuth();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const { displayName, email, photoURL, providerData } = result.user;
+        const users = {
+          name: displayName,
+          email: email,
+          img: photoURL,
+          providerId: providerData[0].providerId
+        };
+        setUser(users);
+
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  }
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        const { displayName, email, photoURL, providerData } = result.user;
+        const users = {
+          name: displayName,
+          email: email,
+          img: photoURL,
+          providerId: providerData[0].providerId
+        };
+        setUser(users);
+
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  }
+
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      setUser({});
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!user.name?
+        <div>
+        <button onClick={handleGoogleSignIn}>Google Sign In</button>
+        <button onClick={handleGithubSignIn}>Github Sign In</button>
+      </div>
+      :<button onClick={handleSignOut}>Sign Out</button>}
+      <br />
+      {
+        user.name && <div>
+          <img src={user.img} alt="" />
+          <h2>Welcome {user.name}</h2>
+          <p>Your email is {user.email?user.email:`not provided by ${user.providerId}`}</p>
+          <p>Data collected from: {user.providerId}</p>
+        </div>
+      }
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyByXnwh_25fZwum43rbRg2jDgxgNES-jEg",
+    authDomain: "react-simple-firebase-auth.firebaseapp.com",
+    projectId: "react-simple-firebase-auth",
+    storageBucket: "react-simple-firebase-auth.appspot.com",
+    messagingSenderId: "362839672299",
+    appId: "1:362839672299:web:6c444f925c5e60b38e45ae",
+    measurementId: "G-CZCQ0GKVWY"
+};
+export default firebaseConfig;
